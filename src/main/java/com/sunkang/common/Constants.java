559c1d031e761767ffb0c45a871a4b974c4cb1ae
Administrator <183657170@qq.com>
2017-04-06 13:52:58 +0800

    常量

---
+++ src/main/java/com/sunkang/common/Constants.java
@@ -0,0 +1,13 @@
+package com.sunkang.common;
+
+/**
+ * @author 孙康
+ * @date 2017/4/6
+ * Describe：保存常量
+ */
+public class Constants {
+    /**
+     * 公众平台填写的token
+     */
+    public final static String TOKEN="sunkang";
+}
---
+++ src/main/java/com/sunkang/common/ErrorCodes.java
@@ -0,0 +1,9 @@
+package com.sunkang.common;
+
+/**
+ * @author 孙康
+ * @date 2017/4/6
+ * Describe：错误码
+ */
+public class ErrorCodes {
+}
